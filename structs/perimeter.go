b504e6d80f678ba56e9bb967b2feae19561518dc
krisctl <krishi87@gmail.com>
2022-03-15 23:43:47 -0400

    removing pointers from receiver function, removing unnecessary fields from function signatures

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -1,14 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func (r *Rectangle) Perimeter(w, h float64) float64 {
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
 
-func (r *Rectangle) Area(w, h float64) float64 {
+func (r Rectangle) Area() float64 {
 	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.radius, 2.0)
 }
--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -2,6 +2,7 @@ package structs
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,14 +12,23 @@ func TestPerimeter(t *testing.T) {
 	var want, got float64
 	want = 44.0
 	r := Rectangle{width: 12.0, height: 10.0}
-	got = r.Perimeter(12.0, 10.0)
+	got = r.Perimeter()
 	assert.Equal(t, want, got, fmt.Sprintf("Not equal, got %.2f, want %.2f", got, want))
 }
 
 func TestArea(t *testing.T) {
-	var want, got float64
-	want = 120.0
-	r := Rectangle{width: 12.0, height: 10.0}
-	got = r.Area(12.0, 10.0)
-	assert.Equal(t, want, got, fmt.Sprintf("Not equal, got %.2f, want %.2f", got, want))
+	t.Run("Area of rectangle", func(t *testing.T) {
+		var want, got float64
+		want = 120.0
+		r := Rectangle{width: 12.0, height: 10.0}
+		got = r.Area()
+		assert.Equal(t, want, got, fmt.Sprintf("Not equal, got %g, want %g", got, want))
+	})
+	t.Run("Area of circle", func(t *testing.T) {
+		var want, got float64
+		want = math.Pi * 100
+		c := Circle{radius: 10.0}
+		got = c.Area()
+		assert.Equal(t, want, got, fmt.Sprintf("Not equal, got %g, want %g", got, want))
+	})
 }
